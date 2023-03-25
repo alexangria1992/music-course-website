887b6490295e16cd5ce86201c2e8c20f51abcdd1
alexangria1992 <alex040892@gmail.com>
2023-03-25 19:53:37 +1300

    Navbar Component

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Navbar from "../Navbar/Navbar";
 import "./Header.css";
 
 const Header = () => {
   return (
     <header>
       <div className="wrapper">
+        <Navbar />
         <div className="cta">
           <p className="course-name">Sound Design Masterclass</p>
           <h1>Learn the Art of Sound Design</h1>
