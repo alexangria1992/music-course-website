79f0be155168db02a9d7a2e8d5d59269b2af8ef1
alexangria1992 <alex040892@gmail.com>
2023-03-25 19:00:13 +1300

    Header Component

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
-  return <div>This is the header</div>;
+  return (
+    <header>
+      <div className="wrapper">
+        <div className="cta">
+          <p className="course-name">Sound Design Masterclass</p>
+          <h1>Learn the Art of Sound Design</h1>
+          <a href="" className="demo-btn">
+            Demo Lesson
+          </a>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
